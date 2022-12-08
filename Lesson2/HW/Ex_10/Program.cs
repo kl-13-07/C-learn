4978cd0ca179da93537c096d7aacbefb277f8a16
NY <oleg.kostyukof@yandex.ru>
2022-12-08 15:37:27 +0300

    Home work lesson2 Ex_10

---
+++ Lesson2/HW/Ex_10/Program.cs
@@ -0,0 +1,28 @@
+﻿using static MyLibrary;
+
+
+
+/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+
+
+int [] array = CreateArray(3);
+FillArray(array, 1, 10);
+PrintArray(array);
+
+Console.WriteLine();
+
+Console.WriteLine(array[1]);
+
+
+/* int[] array={1,2,3};
+
+Console.WriteLine(array[1]);
+ */
+
+
+
