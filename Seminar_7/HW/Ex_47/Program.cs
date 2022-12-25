669c6d5326c05699f44662efbe21192cdaa8c959
NY <oleg.kostyukof@yandex.ru>
2022-12-25 12:21:46 +0300

    Home work Seminar_7!

---
+++ Seminar_7/HW/Ex_47/Program.cs
@@ -0,0 +1,24 @@
+﻿using static MyLibrary;
+
+
+/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9
+ */
+
+
+/* int[,] matrix = new int[3, 5];
+FillArrayX2(matrix, 1, 10);
+PrintArrayX2(matrix); */
+
+double[,] matrix = new double[2, 5];
+
+FillRandDoubleTwo(matrix, 0,  0, true, 100000, 100, 6, true);
+
+PrintArrayX2(matrix);
---
+++ Seminar_7/HW/Ex_50/Program.cs
@@ -0,0 +1,36 @@
+﻿using static MyLibrary;
+
+
+/* Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+17 -> такого числа в массиве нет */
+
+
+int[,] matrix = new int [2, 5];
+FillArrayX2(matrix, 1, 10);
+
+PrintArrayX2 (matrix);
+
+Console.WriteLine("__________________________________________");
+
+Console.WriteLine("\nЗадайте позицию строки, искомого элемента");
+
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("\nЗадайте позицию столбца, искомого элемента");
+
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(matrix[number1, number2]);
+
+//Console.WriteLine($"{matrix [number1, number2]}");
+
+//($" в Ваше массиве {count} чисел больше чем '0' ");
