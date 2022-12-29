63702864948978bee971bdb96796d58dd00da441
NY <oleg.kostyukof@yandex.ru>
2022-12-29 16:44:04 +0300

    Home work Seminar_8!

---
+++ Seminar_8/HW/Ex_54/Program.cs
@@ -0,0 +1,67 @@
+﻿using static MyLibrary;
+
+
+/* Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */
+
+
+int[,] matrix = new int[3, 4];
+
+FillArrayX2(matrix, 1, 10);
+
+
+
+PrintArrayX2(matrix);
+
+
+
+ static void SortMetodx2 (int[,] matr)
+{
+
+for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+
+        int max = matr[i, j];
+
+
+        for (int k = j + 1; k < matr.GetLength(1); k++)
+        {
+
+            if (matr[i, k] > max)
+
+            {
+
+                int temporary = max; 
+                max = matr[i, k];
+                matr[i, k] = temporary;
+            }
+
+        }
+
+        matr[i, j] = max;
+
+
+
+
+    }
+
+
+}
+} 
+
+SortMetodx2(matrix);
+
+Console.WriteLine();
+
+PrintArrayX2(matrix);
+
+
---
+++ Seminar_8/HW/Ex_56/Program.cs
@@ -0,0 +1,103 @@
+﻿using static MyLibrary;
+
+
+
+/* Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка */
+
+
+
+int[,] matrix1 = new int[3, 4];
+
+FillArrayX2(matrix1, 1, 10);
+
+
+int[] Sum1 = CreateArray(matrix1.GetLength(0));
+int minindex1 = 0;
+int min1 = 0;
+
+PrintArrayX2(matrix1);
+Console.WriteLine("Прямоугольный двумерный массив");
+Console.WriteLine("________________________________________________________________________");
+
+
+static void Sumstringx2(int[,] matrix, int[] Sum, int min, int minindex)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+
+    {
+
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Sum[i] += matrix[i, j];
+
+
+
+
+
+
+
+
+        }
+    }
+
+}
+
+
+
+Sumstringx2(matrix1, Sum1, min1, minindex1);
+
+static int CheckMinInArray(int[] Sum, int min, int minindex)
+{
+
+    for (int k = 0; k < Sum.Length; k++)
+    {
+
+        if (k == 0)
+        {
+            min = Sum[k];
+
+            minindex = 0;
+        }
+        else
+        {
+            if (Sum[k] < min)
+            {
+                               
+                minindex = k;
+            }
+        }
+    }
+    return minindex;
+}
+
+PrintArray(Sum1);
+minindex1 = CheckMinInArray(Sum1, min1, minindex1);
+
+Console.WriteLine("Суммы элементов в строках");
+Console.WriteLine("________________________________________________________________________");
+
+Console.WriteLine($"Номер строки с наименьшей суммой элементов : {minindex1}");
+
+
+
+
+
+
+
+
+
+
